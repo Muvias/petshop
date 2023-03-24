98918414787cfdafc7515c473aa394694fbe1ce0
Vinicius Perazza <vi_guepardo@hotmail.com>
2023-03-24 14:39:56 -0300

    menu changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,20 +6,22 @@ export function Header() {
 
     return (
         <header>
-            <a href='/' className="logo">
-                <h1>Li<span>Pethy</span>shop</h1>
-            </a>
+            <div className='headerContainer'>
+                <a href='/' className="logo">
+                    <h1>Li<span>Pethy</span>shop</h1>
+                </a>
 
-            <nav className="menu">
-                <a href='/#inicio' className={location.hash === '#inicio' ? 'active' : ''}>Início</a>
-                <a href='/#sobre' className={location.hash === '#sobre' ? 'active' : ''}>Sobre</a>
-                <a href='/#servicos' className={location.hash === '#servicos' ? 'active' : ''}>Serviços</a>
-                <a href='/#depoimentos' className={location.hash === '#depoimentos' ? 'active' : ''}>Depoimentos</a>
-                <a href='/#contato' className={location.hash === '#contato' ? 'active' : ''}>Contato</a>
-                {/* <a href='/produtos' className={location.pathname === '/produtos' ? 'active' : ''}>Produtos</a> */}
-            </nav>
-
-            <div className="divider"></div>
+                <nav className="menu">
+                    <a href='/#inicio' className={location.hash === '#inicio' ? 'active' : ''}>Início</a>
+                    <a href='/#sobre' className={location.hash === '#sobre' ? 'active' : ''}>Sobre</a>
+                    <a href='/#servicos' className={location.hash === '#servicos' ? 'active' : ''}>Serviços</a>
+                    <a href='/#depoimentos' className={location.hash === '#depoimentos' ? 'active' : ''}>Depoimentos</a>
+                    <a href='/#contato' className={location.hash === '#contato' ? 'active' : ''}>Contato</a>
+                    {/* <a href='/produtos' className={location.pathname === '/produtos' ? 'active' : ''}>Produtos</a> */}
+                </nav>
+                
+                <div className="divider"></div>
+            </div>
         </header>
     )
 }
